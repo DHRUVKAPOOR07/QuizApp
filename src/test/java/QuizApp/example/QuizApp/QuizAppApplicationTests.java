0b24c911c116b9dcd928799757e5c514d8a56d37
DHRUVKAPOOR07 <dhruvkapoor1290@gmail.com>
2025-09-30 14:02:28 +0530

    first time

---
+++ src/test/java/QuizApp/example/QuizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package QuizApp.example.QuizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
